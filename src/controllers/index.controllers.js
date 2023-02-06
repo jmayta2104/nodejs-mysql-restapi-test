d00d3a4cc4c5f8f79b3bf7d26aface5110d1451d
JorgeM <jorgemayta2104@gmail.com>
2023-02-06 17:54:54 -0500

    index.js update

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,6 +1,10 @@
 import { pool } from '../db.js'
 
 export const ping = async (req,res) => {
-    const [result] = await pool.query('SELECT "Pong" AS result') 
-    res.json(result[0])
+    try{
+        const [result] = await pool.query('SELECT "Pong" AS result') 
+        res.json(result[0])
+    }catch(error){
+        res.send(error.message)
+    }
 }
