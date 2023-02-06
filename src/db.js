af57d72dbb03cd8070b86251e75e8c0b94097762
JorgeM <jorgemayta2104@gmail.com>
2023-02-06 17:56:49 -0500

    db.js update

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise'
-import {DB_HOST, DB_PORT, DB_USER, DB_DATABASE} from './config.js'
+import {DB_HOST, DB_PORT, DB_USER, DB_DATABASE, DB_PASSWORD} from './config.js'
 
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
-    password: '',
+    password: DB_PASSWORD,
     port: DB_PORT,
     database: DB_DATABASE
 })
