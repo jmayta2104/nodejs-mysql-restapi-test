0f61c46e3b0af2120e0597e9769dccdb24f1fe8f
JorgeM <jorgemayta2104@gmail.com>
2023-02-08 19:35:24 -0500

    app.js update cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import productoRoutes from './routes/producto.routes.js'
 import indexRoutes from './routes/index.routes.js'
+import cors from 'cors'
 import {PORT} from './config.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(indexRoutes)
